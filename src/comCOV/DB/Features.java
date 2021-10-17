c1052c6e62f3f1e5c13a589e70af03f10ea7b75c
Beet-le <1693332907@qq.com>
2021-10-17 22:52:26 +0800

    Test

--- src/comCOV/DB/Features.java
+++ src/comCOV/DB/Features.java
@@ -52,7 +52,7 @@ public class Features {
         try {
             st = jdbc.opendb();
             int id = sc.nextInt();
-            ResultSet rs = st.executeQuery("select *from UserPassage where userid=" + id + "");
+            ResultSet rs = st.executeQuery("select * from UserPassage where userid="+id+"");
             while (rs.next()) {
                 System.out.print("   姓名:" + rs.getObject("username"));
                 System.out.print("   性别:" + rs.getObject("sex"));
@@ -64,23 +64,29 @@ public class Features {
             e.printStackTrace();
         }
     }
+    @Test
     public void AdministratorSelect() {   //用户查询
         try {
             st = jdbc.opendb();
-            int id = sc.nextInt();
-            ResultSet rs = st.executeQuery("select UserPassage.username ,UserPassage.sex, UserPassage.age, UserPassage.tel ,UserCOV_19test.user_test,UserCOV_19test.user_Diagnosis , UserHospital.user_hospital from UserPassage ,UserCOV_19test ,UserHospitalwhere UserPassage.userid=UserCOV_19test.userid and UserCOV_19test.user_testID=UserHospital.user_testID;");
+            ResultSet rs = st.executeQuery("select UserPassage.username ,UserPassage.sex, UserPassage.age, UserPassage.tel ,UserCOV_19test.user_test,\n" +
+                    "UserCOV_19test.user_Diagnosis , UserHospital.user_hospital ,UserCOV_19test.user_testID\n" +
+                    "from UserPassage ,UserCOV_19test ,UserHospital\n" +
+                    "where UserPassage.userid=UserCOV_19test.userid and UserCOV_19test.user_testID=UserHospital.user_testID");
             while (rs.next()) {
-                System.out.print("   姓名:" + rs.getObject("username"));
-                System.out.print("   性别:" + rs.getObject("sex"));
-                System.out.print("   年龄:" + rs.getObject("age"));
-                System.out.println("   电话:" + rs.getObject("tel"));
-
+                System.out.print("姓名:" + rs.getObject("username")+"  ||   性别:" + rs.getObject("sex"));
+//                System.out.print("  ||   性别:" + rs.getObject("sex"));
+                System.out.print("  ||   年龄:" + rs.getObject("age"));
+                System.out.print("  ||   电话:" + rs.getObject("tel"));
+                System.out.print("  ||  测试方法:"+rs.getObject("user_test"));
+                System.out.print("  ||  确诊状态:"+rs.getObject("user_Diagnosis"));
+                System.out.print("  ||  在院状态:"+rs.getObject("user_hospital"));
+                System.out.println("  ||  检查ID:"+rs.getObject("user_testID"));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+        @Test
     public void add_user() {                //添加用户
         System.out.println("==============>添加用户<===========");
         System.out.println("请输入ID:");
@@ -89,9 +95,15 @@ public class Features {
         String username = sc.next();
         System.out.println("请输入密码");
         String password = sc.next();
+        System.out.println("请输入性别");
+        String sex= sc.next();
+        System.out.println("请输入年龄");
+        int age= sc.nextInt();
+        System.out.println("请输入电话");
+        String tel= sc.next();
         try {
             st = jdbc.opendb();
-            int i = st.executeUpdate("insert into UserPassage(userid,username,password)values(" + stuid + ",'" + username + "','" + password + " ')");
+            int i = st.executeUpdate("insert into UserPassage(userid,username,password,sex,age,tel)values(" + stuid + ",'" + username + "','" + password + " ','"+sex+"',"+age+",'"+tel+"')");
             if (i > 0) {
                 System.out.println("Success");
             } else {
@@ -101,14 +113,12 @@ public class Features {
             e.printStackTrace();
         }
     }
-
     public void del_user() {             //删除用户
         System.out.println("请输入您想删除的用户ID");
         int id = sc.nextInt();
         try {
-
             st = jdbc.opendb();
-            int i = st.executeUpdate("delete from px_stuinfo1 where stuid=" + id + "");
+            int i = st.executeUpdate("delete from UserPassage where stuid=" + id + "");
             if (i > 0) {
                 System.out.println("成功");
             } else {
@@ -118,15 +128,15 @@ public class Features {
             e.printStackTrace();
         }
     }
-
+    @Test
     public void updata_user() {         //修改用户
-        System.out.println("请输入你要修改的用户ID");
+        System.out.println("请输入你要修改的用户测试ID：");
         int id = sc.nextInt();
-        System.out.println("请输入新密码");
-        String pwd = sc.next();
+        System.out.println("修改确诊类型:");
+        String test = sc.next();
         try {
             st = jdbc.opendb();
-            int i = st.executeUpdate("update px_stuinfo1 set password='" + pwd + "' where stuid=" + id + "");
+            int i = st.executeUpdate("update UserCOV_19test set  user_Diagnosis='" + test + "' where user_testID=" + id + "");
             if (i > 0) {
                 System.out.println("成功");
             } else {
--- src/comCOV/Dao/COV19_text.java
+++ src/comCOV/Dao/COV19_text.java
@@ -23,7 +23,7 @@ public class COV19_text {
                     break;
                 case 2:
                     features.add_user();
-                    features.longin();
+
                     cov19_text.userselect();
                     break;
                 case 3:
@@ -37,9 +37,7 @@ public class COV19_text {
                     break;
             }
         } while (Bel);
-
     }
-
     public void userselect() { //普通用户
         features.longin();  //普通用户登录验证
         System.out.print("你的信息:");
@@ -51,16 +49,23 @@ public class COV19_text {
         System.out.println("管理员请登录");
         features.Admlongin();//管理员登录验证
         int print2;
-        boolean Bel = true;
+        boolean Bel2 = true;
         do {
-            System.out.println("1:查看用户 2:修改用户 3:删除用户");
+            System.out.println("1:查看用户 2:修改用户 3:删除用户 4:退出");
             print2 = sc.nextInt();
             switch (print2) {
-                case 1: features.AdministratorSelect();
+                case 1: features.AdministratorSelect() ;
+                break;
+                case 2: features.updata_user();
+                break;
+                case 3:features.del_user();
+                break;
+                case 4: Bel2=false;
+                break;
+                default:
+                    System.out.println("输入错误! 重新输入");
+                    break;
             }
-
-        } while (Bel);
-
+        } while (Bel2);
     }
-
 }
