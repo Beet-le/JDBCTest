531398de7b170e30d376c49a590ce27f1333ac77
Beet-le <1693332907@qq.com>
2021-10-18 17:24:09 +0800

    Test

---
+++ src/com/stx/px/db/UserModel.java
@@ -0,0 +1,49 @@
+package com.stx.px.db;
+
+public class UserModel {
+  private int stuid;
+  private String username;
+  private String sex;
+  private  int age;
+  private  String tel;
+
+    public int getStuid() {
+        return stuid;
+    }
+
+    public void setStuid(int stuid) {
+        this.stuid = stuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
